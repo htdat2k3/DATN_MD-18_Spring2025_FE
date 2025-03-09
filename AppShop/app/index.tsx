2e968f60458b21b6775534f0e433dd980ed4465a
htdat2k3 <dathtph20511@fpt.edu.vn>
2025-03-09 22:21:47 +0700

    update project

---
+++ AppShop/app/index.tsx
@@ -0,0 +1,15 @@
+import { useRef } from "react";
+import { Animated, View } from "react-native";
+
+export default function SplashScreen() {
+    // const fadeAnim = useRef(new Animated.Value(0)).current
+    // const scaleAnim = useRef(new Animated.Value(0.5)).current
+
+    // return <View style = {styles.container}>
+    //     <Animated.View style = {[
+    //         styles.iconContainer
+    //     ]}>
+
+    //     </Animated.View>
+    // </View>
+}
